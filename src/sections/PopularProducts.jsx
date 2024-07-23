3d73b264ec7c50339eee4c3a032aed66500410fe
Qasim <mq12113@gmail.com>
2024-07-23 22:36:40 +0100

    Hero section completed

---
+++ src/sections/PopularProducts.jsx
@@ -0,0 +1,17 @@
+import { products } from "../Components/constants";
+
+const PopularProducts = () => {
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Products</h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          aperiam ratione facilis corporis aliquid.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default PopularProducts;
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+
+}
