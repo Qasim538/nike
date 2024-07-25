210047b3efcecfd03c298efb07ce789810555622
Qasim <mq12113@gmail.com>
2024-07-25 21:42:41 +0100

    updated another section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,10 +1,43 @@
+import { offer } from "../assets/images";
+import Button from "../Components/Button";
+import { arrowRight } from "../assets/icons";
 
 const SpecialOffer = () => {
   return (
-    <div>
-      Special Offer
-    </div>
-  )
-}
+    <section className="flex justify-wrap items-center mx-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img
+          src={offer}
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red"> Special </span>
+          offer
+        </h2>
+        <p className=" mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sunt
+          maiores dolorum minus necessitatibus sed eum quaerat repellat ipsum
+          voluptas.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          our dedication to detail and excellence ensure your satisfaction
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop now" iconURL={arrowRight} />
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
