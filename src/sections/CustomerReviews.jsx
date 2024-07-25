6c6d04d4cd36169b8998dfdd52308026c3ff0cdb
Qasim <mq12113@gmail.com>
2024-07-25 22:48:21 +0100

    customer reviews started

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,9 +1,22 @@
+import {reviews} from '../Components/constants'
+import ReviewCard from "../Components/ReviewCard"
 
 const CustomerReviews = () => {
   return (
-    <div>
-      
+    <section className="max-container">
+    <h3 className="font-palanquin text-center text-4xl font-bold">What Our 
+      <span className="text-coral-red"> Customers </span>
+       Say?
+    </h3>
+    <p className="info-text m-auto mt-4 max-w-lg text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero, aspernatur. Quam modi molestias doloremque nobis!</p>
+
+    <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+    {reviews.map((review) => (
+      <ReviewCard />
+    ))}
     </div>
+      
+    </section>
   )
 }
 
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -4,7 +4,7 @@ import { arrowRight } from "../assets/icons";
 
 const SpecialOffer = () => {
   return (
-    <section className="flex justify-wrap items-center mx-xl:flex-col-reverse gap-10 max-container">
+    <section className="flex flex-col-reverse lg:flex-row justify-between items-center gap-4 lg:gap-10 max-container">
       <div className="flex-1">
         <img
           src={offer}
@@ -14,19 +14,19 @@ const SpecialOffer = () => {
         />
       </div>
       <div className="flex flex-1 flex-col">
-        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
           <span className="text-coral-red"> Special </span>
           offer
         </h2>
-        <p className=" mt-4 lg:max-w-lg info-text">
+        <p className="mt-4 info-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sunt
           maiores dolorum minus necessitatibus sed eum quaerat repellat ipsum
           voluptas.
         </p>
-        <p className="mt-6 lg:max-w-lg info-text">
+        <p className="mt-6 info-text">
           our dedication to detail and excellence ensure your satisfaction
         </p>
-        <div className="mt-11 flex flex-wrap gap-4">
+        <div className="mt-10 mb-10 flex flex-wrap gap-4">
           <Button label="Shop now" iconURL={arrowRight} />
           <Button
             label="Learn more"
