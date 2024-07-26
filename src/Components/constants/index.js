b1efae1d1c19f41baec0326e7d10388c9d7a91db
Qasim <mq12113@gmail.com>
2024-07-26 21:46:15 +0100

    updated customer review section

--- src/Components/constants/index.js
+++ src/Components/constants/index.js
@@ -80,8 +80,14 @@ export const reviews = [
     {
         imgURL: customer2,
         customerName: 'Lota Mongeskar',
-        rating: 4.5,
+        rating: 4.75,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+    },
+    {
+        imgURL: customer1,
+        customerName: 'Richard Brown',
+        rating: 4.25,
+        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     }
 ];
 
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,9 +1,9 @@
 
 const Subscribe = () => {
   return (
-    <div>
-      Subscribe
-    </div>
+    <section className="max-container flex justify-between items-center max-lg:flex-col gap-10">
+
+    </section>
   )
 }
 
